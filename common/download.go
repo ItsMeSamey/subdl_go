95ca2049c7aa716edaad7e50a32ccbe325f7376f
ItsMeSamey <sameychain5041@gmail.com>
2025-03-29 17:02:15 +0530

    added downloader interface

---
+++ common/download.go
@@ -0,0 +1,171 @@
+package common
+
+type LanguageID string
+
+const (
+  LangAN    LanguageID = "an"    // Aragonese
+  LangAR    LanguageID = "ar"    // Arabic
+  LangAT    LanguageID = "at"    // Asturian (non standard?)
+  LangBG    LanguageID = "bg"    // Bulgarian
+  LangBR    LanguageID = "br"    // Breton
+  LangCA    LanguageID = "ca"    // Catalan
+  LangCS    LanguageID = "cs"    // Czech
+  LangDA    LanguageID = "da"    // Danish
+  LangDE    LanguageID = "de"    // German
+  LangEL    LanguageID = "el"    // Greek
+  LangEN    LanguageID = "en"    // English
+  LangEO    LanguageID = "eo"    // Esperanto
+  LangES    LanguageID = "es"    // Spanish
+  LangET    LanguageID = "et"    // Estonian
+  LangEU    LanguageID = "eu"    // Basque
+  LangFA    LanguageID = "fa"    // Persian
+  LangFI    LanguageID = "fi"    // Finnish
+  LangFR    LanguageID = "fr"    // French
+  LangGL    LanguageID = "gl"    // Galician
+  LangHE    LanguageID = "he"    // Hebrew
+  LangHI    LanguageID = "hi"    // Hindi
+  LangHR    LanguageID = "hr"    // Croatian
+  LangHU    LanguageID = "hu"    // Hungarian
+  LangHY    LanguageID = "hy"    // Armenian
+  LangID    LanguageID = "id"    // Indonesian
+  LangIS    LanguageID = "is"    // Icelandic
+  LangIT    LanguageID = "it"    // Italian
+  LangJA    LanguageID = "ja"    // Japanese
+  LangKA    LanguageID = "ka"    // Georgian
+  LangKM    LanguageID = "km"    // Khmer
+  LangKO    LanguageID = "ko"    // Korean
+  LangMK    LanguageID = "mk"    // Macedonian
+  LangMS    LanguageID = "ms"    // Malay
+  LangNL    LanguageID = "nl"    // Dutch
+  LangNO    LanguageID = "no"    // Norwegian
+  LangOC    LanguageID = "oc"    // Occitan
+  LangPTBR  LanguageID = "pt-br" // Portuguese, Brazilian
+  LangPL    LanguageID = "pl"    // Polish
+  LangPT    LanguageID = "pt"    // Portuguese
+  LangRO    LanguageID = "ro"    // Romanian
+  LangRU    LanguageID = "ru"    // Russian
+  LangSI    LanguageID = "si"    // Sinhala
+  LangSK    LanguageID = "sk"    // Slovak
+  LangSL    LanguageID = "sl"    // Slovenian
+  LangSQ    LanguageID = "sq"    // Albanian
+  LangSR    LanguageID = "sr"    // Serbian
+  LangSV    LanguageID = "sv"    // Swedish
+  LangTH    LanguageID = "th"    // Thai
+  LangTL    LanguageID = "tl"    // Tagalog
+  LangTR    LanguageID = "tr"    // Turkish
+  LangTT    LanguageID = "tt"    // Tatar
+  LangUK    LanguageID = "uk"    // Ukrainian
+  LangUZ    LanguageID = "uz"    // Uzbek
+  LangVI    LanguageID = "vi"    // Vietnamese
+  LangZH    LanguageID = "zh"    // Chinese
+  LangZHTW  LanguageID = "zh-tw" // Chinese Traditional
+)
+
+var LanguageNameMap = map[LanguageID]string{
+  LangAN:   "Aragonese",
+  LangAR:   "Arabic",
+  LangAT:   "Asturian",
+  LangBG:   "Bulgarian",
+  LangBR:   "Breton",
+  LangCA:   "Catalan",
+  LangCS:   "Czech",
+  LangDA:   "Danish",
+  LangDE:   "German",
+  LangEL:   "Greek",
+  LangEN:   "English",
+  LangEO:   "Esperanto",
+  LangES:   "Spanish",
+  LangET:   "Estonian",
+  LangEU:   "Basque",
+  LangFA:   "Persian",
+  LangFI:   "Finnish",
+  LangFR:   "French",
+  LangGL:   "Galician",
+  LangHE:   "Hebrew",
+  LangHI:   "Hindi",
+  LangHR:   "Croatian",
+  LangHU:   "Hungarian",
+  LangHY:   "Armenian",
+  LangID:   "Indonesian",
+  LangIS:   "Icelandic",
+  LangIT:   "Italian",
+  LangJA:   "Japanese",
+  LangKA:   "Georgian",
+  LangKM:   "Khmer",
+  LangKO:   "Korean",
+  LangMK:   "Macedonian",
+  LangMS:   "Malay",
+  LangNL:   "Dutch",
+  LangNO:   "Norwegian",
+  LangOC:   "Occitan",
+  LangPTBR: "Brazilian Portuguese",
+  LangPL:   "Polish",
+  LangPT:   "Portuguese",
+  LangRO:   "Romanian",
+  LangRU:   "Russian",
+  LangSI:   "Sinhala",
+  LangSK:   "Slovak",
+  LangSL:   "Slovenian",
+  LangSQ:   "Albanian",
+  LangSR:   "Serbian",
+  LangSV:   "Swedish",
+  LangTH:   "Thai",
+  LangTL:   "Tagalog",
+  LangTR:   "Turkish",
+  LangTT:   "Tatar",
+  LangUK:   "Ukrainian",
+  LangUZ:   "Uzbek",
+  LangVI:   "Vietnamese",
+  LangZH:   "Chinese",
+  LangZHTW: "Traditional Chinese",
+}
+
+type SearchOptions struct {
+  Language      LanguageID
+// SearchOptions interface{}
+}
+type MovieListData struct {
+  // The title of the movie
+  Title   string
+
+  // The Options when frtching the list of subtitles for this movie
+  Options SearchOptions
+}
+type MovieList interface {
+  Data() *MovieListData
+  ToSubtitleLinks() ([]SubtitleList, error)
+}
+
+type SubtitleListData struct {
+  Parent *MovieList
+  // Name of the subtitle file
+  Filename string
+  // Language of the subtitle file
+  Language string
+  // Setting this to "" will force refetching when calling DownloadLink()
+  Target string
+}
+type SubtitleList interface {
+  Data() *SubtitleListData
+  // Weather the downloaded file is a zip file or not
+  IsZip() bool
+  // Returns the Download link from where we can fetch the subtitle file
+  DownloadLink() (string, error)
+  // Downloads the subtitle file and returns the downloaded file
+  Download() (DownloadedFile, error)
+}
+
+type DownloadedSubtitle struct {
+  Subtitles []byte
+  Filename  string
+}
+type DownloadedFileData struct {
+  Parent *SubtitleList
+
+  // May contain 0, or 1 or more subtitles
+  Subtitles []DownloadedSubtitle
+}
+type DownloadedFile interface {
+  Data() *DownloadedFileData
+}
+
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module subtitle_downloader
 
 go 1.24.1
+
+require (
+	github.com/ItsMeSamey/go_utils v1.0.5
+	github.com/PuerkitoBio/goquery v1.10.2
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	golang.org/x/net v0.37.0 // indirect
+)
